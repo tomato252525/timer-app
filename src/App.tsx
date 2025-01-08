70571e9a31f89d3d2ff7131a3ba81312958c6893
tomato252525 <yamagen2510@gmail.com>
2025-01-09 00:57:17 +0900

    marginの調整

--- src/App.tsx
+++ src/App.tsx
@@ -443,7 +443,7 @@ function App() {
           />
         ))}
       </div>
-      <div className="mt-4">
+      <div className="my-4">
         <button
           onClick={() => setIsConfirmModalOpen(true)}
           className="px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-800 transition-colors"
